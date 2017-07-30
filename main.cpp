e66286df575524a8c90ff71a1eafd46572ce6546
eidheim <eidheim@gmail.com>
2017-07-30 15:05:10 +0200

    Updated Simple-WebSocket-Server submodule, and reduced the number of image buffer copies when more than one client is connected. Also reformatted the source code to 2 space indentation

--- main.cpp
+++ main.cpp
@@ -1,32 +1,32 @@
-#include "server_ws.hpp"
 #include "server_http.hpp"
+#include "server_ws.hpp"
 
 #include "process.hpp"
 
 #include <boost/filesystem.hpp>
 
 #include <fstream>
-#include <unordered_set>
 #include <future>
+#include <unordered_set>
 
 using namespace std;
 using namespace TinyProcessLib;
 
-const string ip_address="localhost";
+const string ip_address = "localhost";
 const boost::filesystem::path screenshot_directory(".");
-const string screenshot_filename="screenshot_resized.png";
+const string screenshot_filename = "screenshot_resized.png";
 //ImageMagick commands:
 #if defined(__APPLE__)
-const string screenshot_command("screencapture -x screenshot.png && convert screenshot.png -strip -resize 75% "+screenshot_filename);
+const string screenshot_command("screencapture -x screenshot.png && convert screenshot.png -strip -resize 75% " + screenshot_filename);
 #else
-const string screenshot_command("import -window root -silent -strip -resize 75% "+screenshot_filename);
+const string screenshot_command("import -window root -silent -strip -resize 75% " + screenshot_filename);
 #endif
-const size_t delay=100; //delay in milliseconds
+const size_t delay = 100; //delay in milliseconds
 
 typedef SimpleWeb::SocketServer<SimpleWeb::WS> WsServer;
 typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
 
-const string html=R"RAW(<html>
+const string html = R"RAW(<html>
 <head>
 <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
 <title>Desktop Stream</title>
@@ -37,7 +37,8 @@ const string html=R"RAW(<html>
 <script>
 var ws;
 window.onload=function(){
-  ws=new WebSocket('ws://)RAW"+ip_address+R"RAW(:8080/desktop');
+  ws=new WebSocket('ws://)RAW" +
+                    ip_address + R"RAW(:8080/desktop');
   ws.onmessage=function(evt){
     var blob = new Blob([evt.data], {type: 'application/octet-binary'});
 
@@ -61,139 +62,142 @@ window.onload=function(){
 </html>)RAW";
 
 int main() {
-    HttpServer http_server;
-    http_server.config.port=8080;
-    http_server.io_service=std::make_shared<boost::asio::io_service>(); // To be on the safe side, we create the io_service here. 
-    WsServer ws_server;
-    
-    auto& desktop_endpoint=ws_server.endpoint["^/desktop/?$"];
-    
-    vector<char> image_buffer;
-    vector<char> last_image_buffer;
-    
-    unordered_set<shared_ptr<WsServer::Connection> > connections_receiving, connections_skipped;
-    
-    if(!boost::filesystem::exists(screenshot_directory)) {
-        cerr << screenshot_directory << " does not exist, please create it or change the screenshot directory path" << endl;
-        return 1;
-    }
-    
-    boost::filesystem::path screenshot_path=screenshot_directory/screenshot_filename;
-    if(!boost::filesystem::exists(screenshot_path)) {
-        Process process(screenshot_command, screenshot_directory.string(), nullptr, [](const char *bytes, size_t n) {
-            cerr << string(bytes, n);
-        });
-        if(process.get_exit_status()!=0)
-            return 1;
-    }
-    
-    ifstream ifs;
-    ifs.open(screenshot_path.string(), ifstream::in | ios::binary);
-    ifs.seekg(0, ios::end);
-    size_t length=ifs.tellg();
-    ifs.seekg(0, ios::beg);
-    
-    image_buffer.resize(length);
-    ifs.read(&image_buffer[0], length);
-    
-    ifs.close();
-    
-    thread update_image_thread([&](){
-        while(true) {
-            Process process(screenshot_command, screenshot_directory.string(), nullptr, [](const char *bytes, size_t n) {
-                cerr << string(bytes, n);
-            });
-            if(process.get_exit_status()==0) {
-                promise<void> promise;
-                http_server.io_service->post([&] {
-                    ifs.open(screenshot_path.string(), ifstream::in | ios::binary);
-                    ifs.seekg(0, ios::end);
-                    size_t length=ifs.tellg();
-                    ifs.seekg(0, ios::beg);
-                    
-                    image_buffer.resize(length);
-                    ifs.read(&image_buffer[0], length);
-                    bool equal_buffer=(image_buffer==last_image_buffer);
-                    if(!equal_buffer)
-                        last_image_buffer=image_buffer;
-                    
-                    ifs.close();
-                    
-                    unordered_set<shared_ptr<WsServer::Connection> > connections;
-                    if(equal_buffer)
-                        connections=connections_skipped;
-                    else
-                        connections=desktop_endpoint.get_connections();
-                    for(auto &a_connection: connections) {
-                        bool skip_connection=false;
-                        if(connections_receiving.count(a_connection)>0) {
-                            skip_connection=true;
-                            connections_skipped.emplace(a_connection);
-                        }
-                        
-                        if(!skip_connection) {
-                            connections_skipped.erase(a_connection);
-                            
-                            auto send_stream=make_shared<WsServer::SendStream>();
-                            
-                            send_stream->write(image_buffer.data(), image_buffer.size());
-                            
-                            connections_receiving.emplace(a_connection);
-                            a_connection->send(send_stream, [&connections_receiving, a_connection](const boost::system::error_code &ec) {
-                                connections_receiving.erase(a_connection);
-                            }, 130);
-                        }
-                    }
-                    promise.set_value();
-                });
-                promise.get_future().wait();
-            }
-            this_thread::sleep_for(chrono::milliseconds(delay));
-        }
+  HttpServer http_server;
+  http_server.config.port = 8080;
+  http_server.io_service = std::make_shared<boost::asio::io_service>(); // To be on the safe side, we create the io_service here.
+  WsServer ws_server;
+
+  auto &desktop_endpoint = ws_server.endpoint["^/desktop/?$"];
+
+  vector<char> image_buffer;
+
+  unordered_set<shared_ptr<WsServer::Connection>> connections_receiving, connections_skipped;
+
+  if(!boost::filesystem::exists(screenshot_directory)) {
+    cerr << screenshot_directory << " does not exist, please create it or change the screenshot directory path" << endl;
+    return 1;
+  }
+
+  boost::filesystem::path screenshot_path = screenshot_directory / screenshot_filename;
+  if(!boost::filesystem::exists(screenshot_path)) {
+    Process process(screenshot_command, screenshot_directory.string(), nullptr, [](const char *bytes, size_t n) {
+      cerr << string(bytes, n);
     });
-    
-    desktop_endpoint.on_open=[&](shared_ptr<WsServer::Connection> connection) {
-        auto send_stream=make_shared<WsServer::SendStream>();
-        
-        send_stream->write(image_buffer.data(), image_buffer.size());
-        
-        connections_receiving.emplace(connection);
-        connection->send(send_stream, [&connections_receiving, connection](const boost::system::error_code &ec) {
-            connections_receiving.erase(connection);
-        }, 130);
-    };
-    
-    desktop_endpoint.on_close=[&](shared_ptr<WsServer::Connection> connection, int status, const string& reason) {
-        connections_receiving.erase(connection);
-        connections_skipped.erase(connection);
-    };
-    
-    desktop_endpoint.on_error=[&](shared_ptr<WsServer::Connection> connection, const boost::system::error_code& ec) {
-        connections_receiving.erase(connection);
-        
-        connections_skipped.erase(connection);
-        
-        cerr << "Error: " << ec << ", error message: " << ec.message() << endl;
-    };
-    
-    http_server.default_resource["GET"]=[](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
-        *response << "HTTP/1.1 200 OK\r\nContent-Length: " << html.size() << "\r\n\r\n" << html;
-    };
-    
-    http_server.on_upgrade=[&ws_server](unique_ptr<SimpleWeb::HTTP> &socket, shared_ptr<HttpServer::Request> request) {
-        auto connection=std::make_shared<WsServer::Connection>(std::move(socket));
-        connection->method=std::move(request->method);
-        connection->path=std::move(request->path);
-        connection->http_version=std::move(request->http_version);
-        connection->header=std::move(request->header);
-        connection->remote_endpoint_address=std::move(request->remote_endpoint_address);
-        connection->remote_endpoint_port=request->remote_endpoint_port;
-        ws_server.upgrade(connection);
-    };
-    
-    http_server.start();
-    http_server.io_service->run();
-    update_image_thread.join();
-    
-    return 0;
+    if(process.get_exit_status() != 0)
+      return 1;
+  }
+
+  ifstream ifs;
+  ifs.open(screenshot_path.string(), ifstream::in | ios::binary);
+  ifs.seekg(0, ios::end);
+  size_t length = ifs.tellg();
+  ifs.seekg(0, ios::beg);
+
+  image_buffer.resize(length);
+  ifs.read(&image_buffer[0], length);
+
+  ifs.close();
+
+  thread update_image_thread([&]() {
+    vector<char> last_image_buffer;
+    std::shared_ptr<WsServer::SendStream> send_stream;
+    while(true) {
+      Process process(screenshot_command, screenshot_directory.string(), nullptr, [](const char *bytes, size_t n) {
+        cerr << string(bytes, n);
+      });
+      if(process.get_exit_status() == 0) {
+        promise<void> promise;
+        http_server.io_service->post([&] {
+          ifs.open(screenshot_path.string(), ifstream::in | ios::binary);
+          ifs.seekg(0, ios::end);
+          size_t length = ifs.tellg();
+          ifs.seekg(0, ios::beg);
+
+          image_buffer.resize(length);
+          ifs.read(&image_buffer[0], length);
+          bool equal_buffer = (image_buffer == last_image_buffer);
+          if(!equal_buffer) {
+            last_image_buffer = image_buffer;
+            send_stream=make_shared<WsServer::SendStream>();
+            send_stream->write(image_buffer.data(), image_buffer.size());
+          }
+
+          ifs.close();
+          
+          unordered_set<shared_ptr<WsServer::Connection>> connections;
+          if(equal_buffer)
+            connections = connections_skipped;
+          else
+            connections = desktop_endpoint.get_connections();
+          
+          for(auto &a_connection : connections) {
+            bool skip_connection = false;
+            if(connections_receiving.count(a_connection) > 0) {
+              skip_connection = true;
+              connections_skipped.emplace(a_connection);
+            }
+
+            if(!skip_connection) {
+              connections_skipped.erase(a_connection);
+
+              connections_receiving.emplace(a_connection);
+              
+              a_connection->send(send_stream, [&connections_receiving, a_connection](const boost::system::error_code &ec) {
+                connections_receiving.erase(a_connection);
+              }, 130);
+            }
+          }
+          promise.set_value();
+        });
+        promise.get_future().wait();
+      }
+      this_thread::sleep_for(chrono::milliseconds(delay));
+    }
+  });
+
+  desktop_endpoint.on_open = [&](shared_ptr<WsServer::Connection> connection) {
+    auto send_stream = make_shared<WsServer::SendStream>();
+
+    send_stream->write(image_buffer.data(), image_buffer.size());
+
+    connections_receiving.emplace(connection);
+    connection->send(send_stream, [&connections_receiving, connection](const boost::system::error_code &ec) {
+      connections_receiving.erase(connection);
+    }, 130);
+  };
+
+  desktop_endpoint.on_close = [&](shared_ptr<WsServer::Connection> connection, int status, const string &reason) {
+    connections_receiving.erase(connection);
+    connections_skipped.erase(connection);
+  };
+
+  desktop_endpoint.on_error = [&](shared_ptr<WsServer::Connection> connection, const boost::system::error_code &ec) {
+    connections_receiving.erase(connection);
+
+    connections_skipped.erase(connection);
+
+    cerr << "Error: " << ec << ", error message: " << ec.message() << endl;
+  };
+
+  http_server.default_resource["GET"] = [](shared_ptr<HttpServer::Response> response, shared_ptr<HttpServer::Request> request) {
+    *response << "HTTP/1.1 200 OK\r\nContent-Length: " << html.size() << "\r\n\r\n"
+              << html;
+  };
+
+  http_server.on_upgrade = [&ws_server](unique_ptr<SimpleWeb::HTTP> &socket, shared_ptr<HttpServer::Request> request) {
+    auto connection = std::make_shared<WsServer::Connection>(std::move(socket));
+    connection->method = std::move(request->method);
+    connection->path = std::move(request->path);
+    connection->http_version = std::move(request->http_version);
+    connection->header = std::move(request->header);
+    connection->remote_endpoint_address = std::move(request->remote_endpoint_address);
+    connection->remote_endpoint_port = request->remote_endpoint_port;
+    ws_server.upgrade(connection);
+  };
+
+  http_server.start();
+  http_server.io_service->run();
+  update_image_thread.join();
+
+  return 0;
 }
